570a819ef5fd81ade44dce92e01535a86ca416f9
Tauke Abdrakhmanov <tauke1994@gmail.com>
2021-02-24 20:08:46 +0600

    Удалил ненужный код

--- interface/cache/cache_client.go
+++ interface/cache/cache_client.go
@@ -1,11 +1,8 @@
 package cache
 
 import (
-	"errors"
 	"time"
 )
-
-var notFoundError = "key not found"
 
 type CacheClient interface {
 	GetString(key string) (string, error)
@@ -13,16 +10,4 @@ type CacheClient interface {
 	SetString(key, value string, duration time.Duration) error
 	Set(key string, object interface{}, duration time.Duration) error
 	Delete(key string) error
-}
-
-func IsKeyNotFound(err error) bool {
-	if err == nil {
-		return false
-	}
-
-	return err.Error() == notFoundError
-}
-
-func CreateKeyNotFoundError() error {
-	return errors.New(notFoundError)
 }
